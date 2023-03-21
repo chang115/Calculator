2934f3749b0314e711dd6536633e1d076e04f8c8
Evren Yaman <yamane@bu.edu>
2023-03-21 23:36:40 +0000

    Called and added the dummy function that effectively calls the AND, OR, and SUM symbols

--- calc.s
+++ calc.s
@@ -1,13 +1,21 @@
 	.intel_syntax noprefix
 	.text
 	.global _start
+comp:
+	cmp rbx, '|'
+	je OR_FRAG
+	cmp rbx, '&'
+	je AND_FRAG
+	cmp rbx, 'S'
+	je SUM_FRAG
+	ret
 _start:
 	xor rax, rax
 	mov rbx, OFFSET [CALC_DATA_BEGIN]
 loop_start:	
 	cmp rbx, 0
 	jz loop_done
-	add rbx, 8
+	call comp
 	jmp loop_start
 loop_done:
 	syscall
