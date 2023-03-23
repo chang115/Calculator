7054567caf1061873b523610ba127073dbc14434
Evren Yaman <yamane@bu.edu>
2023-03-23 20:26:10 +0000

    aded or and and sum dummy functions in order to organize code and call or and and sum symbols

--- calc.s
+++ calc.s
@@ -1,13 +1,25 @@
 	.intel_syntax noprefix
 	.text
 	.global _start
+and:
+	add rbx, 8
+	call AND_FRAG
+	ret
+or:
+	add rbx, 8
+	call OR_FRAG
+	ret
+sum:
+	add rbx, 8
+	call SUM_FRAG
+	ret
 comp:
 	cmp rbx, '|'
-	je OR_FRAG
+	je or
 	cmp rbx, '&'
-	je AND_FRAG
+	je and
 	cmp rbx, 'S'
-	je SUM_FRAG
+	je sum
 	ret
 _start:
 	xor rax, rax
