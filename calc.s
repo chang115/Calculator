4b37465113003e77622c65fe5b5241210663a85a
Evren Yaman <yamane@bu.edu>
2023-03-23 21:19:34 +0000

    FIXED CALC.S STEP 2/3

--- calc.s
+++ calc.s
@@ -1,31 +1,31 @@
 	.intel_syntax noprefix
 	.text
 	.global _start
-and:
+A:
 	add rbx, 8
 	call AND_FRAG
 	ret
-or:
+O:
 	add rbx, 8
 	call OR_FRAG
 	ret
-sum:
+S:
 	add rbx, 8
 	call SUM_FRAG
 	ret
 comp:
-	cmp rbx, '|'
-	je or
-	cmp rbx, '&'
-	je and
-	cmp rbx, 'S'
-	je sum
+	cmp QWORD PTR[rbx], '|'
+	je O
+	cmp QWORD PTR[rbx], '&'
+	je A
+	cmp QWORD PTR[rbx], 'S'
+	je S
 	ret
 _start:
 	xor rax, rax
 	mov rbx, OFFSET [CALC_DATA_BEGIN]
 loop_start:	
-	cmp rbx, 0
+	cmp QWORD PTR[rbx], 0
 	jz loop_done
 	call comp
 	jmp loop_start
