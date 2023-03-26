b1b3e205e467001256d08297ae2a092149f7e9fb
Ethan Chang <chang115@bu.edu>
2023-03-26 03:01:14 +0000

    FINALLY FIXED UPPER.S SO HAPPY

--- upper.s
+++ upper.s
@@ -2,22 +2,24 @@
 	.section .text
 	.global UPPER_FRAG
 comp:
-	cmp BYTE PTR[rbx], 'z'
+	cmp BYTE PTR[rcx], 'z'
 	jle ite
 	
 	
 UPPER_FRAG:
-	cmp BYTE PTR[rbx], 0
+	mov rcx, QWORD PTR[CALC_DATA_BEGIN+8]
+loop_start:	
+	cmp BYTE PTR[rcx], 0
 	je loop_done
 	inc rax
-	cmp BYTE PTR[rbx], '`'
+	cmp BYTE PTR[rcx], '`'
 	jg comp
-	add QWORD PTR[rbx], 1
-	jmp UPPER_FRAG
+	add rcx, 1
+	jmp loop_start
 ite:
-	sub BYTE PTR[rbx], 0x20
-	add QWORD PTR[rbx], 1
-	jmp UPPER_FRAG
+	sub BYTE PTR[rcx], 0x20
+	add rcx, 1
+	jmp loop_start
 
 loop_done:
 	add rbx, 8
